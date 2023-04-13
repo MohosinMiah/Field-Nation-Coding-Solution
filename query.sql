aeef535ee30aa8f62b72fbbfc6ce974aef414232
Md Mohosin Miah <32362094+MohosinMiah@users.noreply.github.com>
2023-04-14 00:38:30 +0600

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,14 @@
+SELECT
+    u.`user_id`,
+    u.`first_name`,
+    u.`last_name`,
+    AVG( tr.`correct` ) AS averageCorrectAnswer,
+    MAX( tr.`time_taken` ) AS recentExamTaken
+FROM
+    `user` AS u
+    LEFT JOIN test_result AS tr ON u.user_id = tr.user_id
+GROUP BY
+    u.`user_id`
+ORDER BY
+    u.`user_id` ASC
+;
